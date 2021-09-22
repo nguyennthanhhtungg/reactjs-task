0d5f73e23e8e03c35a5eb13fbaf2f2f9f6d9a401
nguyenthanhtung884318@gmail.com <nguyenthanhtung884318@gmail.com>
2021-09-22 14:19:23 +0700

    (feat): change timeout for database call from 5000 to 10000

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 //'https://shoponlinetask.herokuapp.com/api',
 export const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_HOST,
-  timeout: 5000,
+  timeout: 10000,
   withCredentials: true
 });
